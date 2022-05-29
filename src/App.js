7f48497a40e33f517b2d882649b1118ebc140678
haktan-tekin <webhaktan@gmail.com>
2022-05-29 13:21:32 +0300

    react-router-dom added and switch page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
-import Users from './components/Users';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
+
 
 function App() {
   return (
-    <div className="App">
-    <Users />
-    </div>
+    <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/users" element={<Users />}>
+            </Route>
+            <Route exact path="/" element={<Home />}>
+            </Route>
+          </Routes>
+        </div>
+    </Router>
   );
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react"
-import axios from "axios";
-
+import React from 'react'
 
 function Users() {
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        axios("https://jsonplaceholder.typicode.com/users")
-        .then((resp) => setUsers(resp.data))
-        .catch((e) => console.log(e))
-        .finally(() => setIsLoading(false))
-    },[])
-
   return (
-    <div>
-        <h1>Users</h1>
-        { isLoading &&  <div>Loading...</div>}
-
-        {
-            users.map(user => <div key={user.id}>{user.name}</div>)
-        }
-    </div>
+    <div>Users</div>
   )
 }
 
